cef4f754877a060e8fc422fc04e9485fc954bac6
Wafick <wafickachiambi@gmail.com>
2026-06-18 16:13:01 -0400

    update buffer code and hypers

--- baseline/main.py
+++ baseline/main.py
@@ -14,13 +14,16 @@ from dataclasses import dataclass
 import mlflow
 
 
-# - hypers 
+MAX_EP_STEPS = 500
 NUM_ENVS = 2
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def vec_env():
     def make():
-        x = gym.make("ALE/MsPacman-v5")
-        # - norm etc
+        x = gym.make("ALE/MsPacman-v5",max_episode_steps=MAX_EP_STEPS)
+        # TODO
+        # grayscale
+        # frame stack,skip,obs reshape
         return x
     return AsyncVectorEnv([make for _ in range(NUM_ENVS)])
 
@@ -38,23 +41,37 @@ class q_function(nn.Module):
     def __init__(self):
         pass
 
-    def forward(self): # q(s,a) -> q value
+    def forward(self,s,a): # q(s,a) -> q value
         return None
 
 
 class buffer:
-    def __init__(self):
-        pass
+    def __init__(self,env=None,policy=None,q_function=None):
+        self.env = env
+        self.policy = policy
+        self.q_functin = q_function
+        
+        self.b_cur_states = torch.zeros(NUM_ENVS,210,160,3,dtype=torch.half,device=DEVICE) # TODO : squeeze -1 dim
+        self.b_nx_states = torch.zeros(NUM_ENVS,210,160,3,dtype=torch.half,device=DEVICE)
+        self.b_reward = torch.zeros(NUM_ENVS,1,dtype=torch.half,device=DEVICE) # TODO unsqueeze -1
+        self.done = torch.zeros(NUM_ENVS,1,dtype=torch.bool,device=DEVICE)
+
+        self.step_num = 0
 
     def step(self):
-        pass
-
+        self.step_num+=1
+        
+        self.env.reset()
+        action = self.env.action_space.sample()
+        state,reward,done,trunc,info = self.env.step(action)
+           
     def sample(self,batch):
         pass
 
+
 class ddqn:
     def __init__(start=False):
-        pass
+        self.env = vec_env()
     
     def save(self):
         pass
@@ -74,5 +91,7 @@ class ddqn:
         
 
 if __name__ == "__main__":
-    ddqn().run(True,storage_path="./")
+    #ddqn().run(True,storage_path="./")
+    env = vec_env()
+    buffer(env).step()
     
