e85caec50371a49cf79319fbb06927f77d632502
Wafick <wafickachiambi@gmail.com>
2026-06-27 20:06:26 -0400

    implemented prototype of Hasselt 2015 with polyak averaging to update the target network

--- baseline/2015.py
+++ baseline/2015.py
@@ -2,8 +2,7 @@
 van Hasselt et al.(2015)
 
 environment : Ms PacMan
-action space : MultiDiscrete([9 9])
-
+https://ale.farama.org/environments/ms_pacman/
 """
 import gymnasium as gym 
 import ale_py
@@ -11,12 +10,10 @@ from gymnasium.vector.async_vector_env import AsyncVectorEnv
 from gymnasium.wrappers.transform_observation import GrayscaleObservation,ResizeObservation
 
 import numpy as np
-import torch,sys,random,mlflow
+import torch,sys,random
 import torch.nn as nn
 import torch.nn.functional as F
-from torch import tensor
 from torch.distributions import Categorical
-from torch.optim import Adam
 
 from copy import deepcopy
 from collections import deque
@@ -26,26 +23,26 @@ from tqdm import tqdm
 
 
 MAX_EP_STEPS = 500
-NUM_ENVS = 20
+NUM_ENVS = 2#20
 R_SHAPE = (150,150)
 # -
+EPSILON = 0.1
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 MAX_STEPS = 2_000 
 GAMMA = .99
-LR = None 
+LR = int(2e-4)
+TAU = 0.5
 
-# TODO : change env to frozen lake
+
 def vec_env():
     def make():
         x = gym.make("ALE/MsPacman-v5",max_episode_steps=MAX_EP_STEPS)
         x = GrayscaleObservation(x)
-        x = ResizeObservation(x,R_SHAPE)
-        # TODO : frame stack,skip,obs reshape
+        x = ResizeObservation(x,R_SHAPE) 
         return x # [1, 150, 150]
     return AsyncVectorEnv([make for _ in range(NUM_ENVS)])
 
 
-# TODO : remove gradient method
 class q_function(nn.Module):
     def __init__(self):  
         super().__init__()
@@ -73,80 +70,76 @@ class q_function(nn.Module):
 
 
 class ddqn:
-    def __init__(self,start = False,storage_path=None):
-        self.start = start
+    def __init__(self,storage_path=None):
         self.env = vec_env()
   
         q_function()(torch.randint(0,1255,(NUM_ENVS,1,150,150),dtype=torch.float,device=DEVICE))
         self.q1 = q_function()
-        self.q2 = deepcopy(self.q1)
+        self.target_net = deepcopy(self.q1)
         self.buffer = deque(maxlen=MAX_EP_STEPS)
 
-        self.q1.to(DEVICE) ; self.q2.to(DEVICE) 
-        self.q1.compile()  ; self.q2.compile()
+        self.q1.to(DEVICE) 
+        self.target_net.to(DEVICE) 
+        # self.q1.compile()
+        # self.target_net.compile()
 
-        self.optim = Adam(chain(self.q1.parameters(),self.q2.parameters()),lr=LR)
+        self.optim = torch.optim.Adam(chain(self.q1.parameters(),self.target_net.parameters()),lr=LR)
+        self.to_tensor = lambda x : torch.tensor(x,dtype=torch.float,device=DEVICE)
     
-    # TODO remove save function
     def save(self,storage_path):
         data = {
             "q1 state":self.q1.state_dict(),
-            "q2 state":self.q2.state_dict(),
+            "target net state":self.q2.state_dict(),
             "optim state":self.optim.state_dict()
         }
         torch.save(data,f"{storage_path}/state_{n}.pth")
     
-    def q_update(self,transition,q1,q2):
-        with torch.no_grad():
-            state,nx_state,reward,done,action = transition
-            
-            # TODO : fix formula
-            argmax = torch.argmax(q1(nx_state),1)
-            disc_q2 = GAMMA * q2(nx_state) # discounted q2 value
-            target = (reward + disc_q2.mean(-1) * (1-done.float())).unsqueeze(-1) 
-        
-        new_q = q1(state)
-        new_q = torch.gather(new_q,1,action.unsqueeze(-1))
+    def main(self):
+        self.state = torch.tensor(self.env.reset()[0],dtype=torch.float,device=DEVICE).unsqueeze(1)
+        for n in tqdm(range(MAX_STEPS),total=MAX_STEPS):
 
-        loss = F.smooth_l1_loss(new_q,target).mean()
-        self.optim.zero_grad(set_to_none=True)
-        loss.backward()
-        self.optim.step()
-        return loss.item()
+            for i in range(MAX_EP_STEPS):
+                with torch.no_grad():
+                    if random.random() < EPSILON : action = self.env.action_space.sample()
+                    else : action = torch.argmax(self.q1(self.state),dim=1).tolist()
+                    
+                    nx_state,reward,done,trunc,_ = self.env.step(action)
+                    
+                    data = [self.state,nx_state,reward,done] 
+                    data = list(map(self.to_tensor,data))
+                    self.buffer.append(data)
+                    
+                    self.state = torch.tensor(nx_state,dtype=torch.float,device=DEVICE).unsqueeze(1)
+                    # todo : handle end of episodes transitions
 
-    def run(self):
-        if self.start:
-            with mlflow.start_run() as run:
-                self.state = tensor(self.env.reset()[0],dtype=torch.float,device=DEVICE).unsqueeze(1)
+            for t in range(MAX_EP_STEPS):
+                id_ = random.randint(0,MAX_EP_STEPS)
+                s,nx,r,d = self.buffer[id_] # s : state, nx : state t+1, r : reward, d : done
+                nx = nx.unsqueeze(1)
+                a = torch.argmax(self.q1(nx),1).unsqueeze(0) 
                 
-                for n in tqdm(range(MAX_STEPS),total=MAX_STEPS):
-                    #TODO : FIX LOOP LOGIC AND ADD EGREEDY
-                    for i in range(MAX_EP_STEPS):
-                        with torch.no_grad():
-                            action = torch.argmax(self.q1(self.state) + self.q2(self.state),dim=1)
-                            nx_state,reward,done,trunc,info = self.env.step(action.long().tolist())
-                            self.buffer.append(
-                                [
-                                    self.state,
-                                    tensor(nx_state,dtype=torch.float,device=DEVICE).unsqueeze(1),
-                                    tensor(reward,device=DEVICE),
-                                    tensor(done,device=DEVICE),
-                                    action
-                                ]
-                            )
-                            self.state = tensor(nx_state,dtype=torch.float,device=DEVICE).unsqueeze(1) 
-                            mlflow.log_metrics({"reward":reward.mean().item()},step=n)
-                        
-                        #TODO MOVE TO SOFT UPDATE
-                        """
-                        for transition in self.buffer:
-                            if
-                                loss = self.q_update(transition,self.q1,self.q2)
-                            else:
-                                loss = self.q_update(transition,self.q2,self.q1)
-                            mlflow.log_metrics({"loss":loss},step=n)
-                        """
-                if n%100== 0:
-                    self.save(n) # state dicts saving
+                q_target_values = self.target_net(nx) # eval of a using target net
+                eval_ = torch.gather(q_target_values,1,a)
+                target = r + (GAMMA * eval_ * (1-d))
+
+                pred = torch.argmax(self.q1(s),1).float()
+
+                loss = F.mse_loss(pred,target).mean()
+      
+                self.optim.zero_grad(set_to_none=True)
+                loss.backward()
+                self.optim.step()
+                
+                # polyak averaging
+                for q1_params,q_target_params in zip(self.q1.parameters(),self.target_net.parameters()):
+                    q_target_params.data.mul_(1.0 - TAU).add_(q1_params.data,alpha=TAU)
+    
+            if n%100== 0:
+                self.save(n) # state dicts saving
 
 
+
+if __name__ == "__main__":
+    import warnings
+    warnings.filterwarnings("ignore")
+    ddqn().main()
