8d0558e8a2338867a2cbcf850bbcbe672db746e8
Wafick <wafickachiambi@gmail.com>
2026-07-06 23:14:57 -0400

    add skeleton

---
+++ finrl_ddqn/main.py
@@ -0,0 +1,42 @@
+from env import __build_train_env
+
+import torch,sys,random,mlflow
+import torch.nn as nn
+import torch.nn.functional as F
+
+from copy import deepcopy
+from collections import deque
+from itertools import chain
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+
+env = __build_train_env()
+
+
+class lstm(nn.Module):
+    def __init__():
+        super().__init__()
+        pass
+
+    def forward():
+        pass
+
+
+class ddqn:
+    def __init__(self):
+        pass
+
+    def save():
+        pass
+
+    def run(self):
+        pass
+
+    def test(self):
+        pass
+
+
+if __name__=="__main__":
+    ddqn().run()
+
