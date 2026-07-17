cd137b321314d5ce1b48c1dc9c0dadfa33de75c6
adeotti <wafickachiambi@gmail.com>
2026-07-17 01:38:47 -0400

    Add preallocated tensor for data collection and remove the for loops in the sample method,gained around 35ms per iterations haha

--- baseline/2015.py
+++ baseline/2015.py
@@ -23,10 +23,10 @@ from queue import Queue
 
 
 MAX_EP_STEPS = 500
-NUM_ENVS = 10
+NUM_ENVS = 3
 R_SHAPE = (100,100)
 # -
-DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 WARMUP_STEPS = 100
 MAX_STEPS = int(35e5)
 GAMMA = .99
@@ -78,9 +78,9 @@ class ddqn:
     def __init__(self,storage_path=None):
         self.storage_path = storage_path
         self.env = vec_env()
-        channels = self.env.observation_space.shape[1]
+        self.channels = self.env.observation_space.shape[1]
     
-        q_function()(torch.randint(0,255,(NUM_ENVS,channels,*R_SHAPE),dtype=torch.float))
+        q_function()(torch.randint(0,255,(NUM_ENVS,self.channels,*R_SHAPE),dtype=torch.float))
         self.q1 = q_function()
         self.target_net = deepcopy(self.q1)
         self.buffer = deque(maxlen=MAX_EP_STEPS)
@@ -93,6 +93,7 @@ class ddqn:
         self.optim = torch.optim.Adam(chain(self.q1.parameters(),self.target_net.parameters()),lr=LR)
         self.reward_data = torch.zeros(NUM_ENVS,dtype=torch.float)
         self.step_count = 0
+        self.init_storage() # !!
 
         # thread 1 -> step env and collecting transitions
         self.queue = Queue(maxsize=10000)
@@ -103,6 +104,15 @@ class ddqn:
         self.thread_2 = Thread(target=self.sample,args=(self.queue,),daemon=False)
         self.gpu_stream = Queue(maxsize=10000)
 
+    
+    def init_storage(self):
+        b = 503
+        self.t_state = torch.zeros(b,NUM_ENVS,self.channels,*R_SHAPE)
+        self.t_nx_state = torch.zeros(b,NUM_ENVS,self.channels,*R_SHAPE)
+        self.t_reward = torch.zeros(b,NUM_ENVS)
+        self.t_done = torch.zeros(b,NUM_ENVS)
+        self.t_action = torch.zeros(b,NUM_ENVS)
+
 
     def save(self,n): 
         data = { "q1 state":self.q1.state_dict(),
@@ -112,16 +122,10 @@ class ddqn:
         torch.save(data,f"{self.storage_path}/state_{n}.pth")
 
 
-    def stack__(self,x):
-        x = [torch.tensor(item, dtype=torch.float) for item in x]
-        x = torch.stack(x)
-        return x
-
-
     @torch.no_grad()
     def step_env(self,queue):
-        data_list = []
-        for n in range(MAX_STEPS):
+        n = 0
+        for t in range(MAX_STEPS):
             if self.step_count == 0:
                 self.state = torch.tensor(self.env.reset()[0],dtype=torch.float,device="cpu")
 
@@ -136,48 +140,41 @@ class ddqn:
             nx_state,reward,done,trunc,_ = self.env.step(action)
             self.reward_data += reward
             
-            data_list.append((self.state,nx_state,reward,done,action))
+            #data_list.append((self.state,nx_state,reward,done,action))
+            self.t_state[n].copy_(torch.tensor(self.state))
+            self.t_nx_state[n].copy_(torch.tensor(nx_state))
+            self.t_reward[n].copy_(torch.tensor(reward))
+            self.t_done[n].copy_(torch.tensor(done))
+            self.t_action[n].copy_(torch.tensor(action))
+            
+            n+=1
+
             if n > 0  and n % MAX_EP_STEPS == 0 :
-                queue.put((data_list))
+                data = (self.t_state,self.t_nx_state,self.t_reward,self.t_done,self.t_action)
+                queue.put(data)
 
                 if not self.thread_2.is_alive(): # start thread 2 after the first episode data collection
                     self.thread_2.start()
-                    
-                data_list = []
+                
+                n = 0
+                self.init_storage()
                                                 
             self.state = torch.tensor(nx_state,dtype=torch.float,device="cpu")
             self.step_count += 1
-
+            
 
     def sample(self,queue):
-        while True:
-            episode = queue.get() # episode should be lenght MAX_EP_STEPS
-            
-            # s : state , nx : next state , r : reward , d : done , a : actions
-            s = [item[0] for item in episode]
-            nx = [item[1] for item in episode]
-            r = [item[2] for item in episode]
-            d = [item[3] for item in episode]
-            a = [item[4] for item in episode]
-            
-            # batching
-            b_state = self.stack__(s).flatten(0,1) 
-            b_nx_state = self.stack__(nx).flatten(0,1)
-
-            b_reward = self.stack__(r).reshape(-1)    # (steps*envs,)
-            b_done   = self.stack__(d).reshape(-1)    # (steps*envs,)
-            b_action = self.stack__(a).reshape(-1, 1) # (steps*envs,1)
-            
-            # random sampling
+        while True: 
+            t_state,t_nx_state,t_reward,t_done,t_action = queue.get()
+                
             idx = torch.randperm(BATCH_SIZE)
-            s_state = b_state[idx].to(DEVICE)
-            s_nx_state = b_nx_state[idx].to(DEVICE)
-            s_reward = b_reward[idx].to(DEVICE)
-            s_done = b_done[idx].to(DEVICE)
-            s_action = b_action[idx].long().to(DEVICE)
-
+            s_state = t_state[idx].flatten(0,1)
+            s_nx_state = t_nx_state[idx].flatten(0,1)
+            s_reward = t_reward[idx].reshape(-1)          # (steps*envs,)
+            s_done = t_done[idx].reshape(-1)              # (steps*envs,)
+            s_action = t_action[idx].long().reshape(-1,1) # (steps*envs,1)
+            
             sample = (s_state,s_nx_state,s_reward,s_done,s_action)
-
             self.gpu_stream.put(sample) 
             
     
@@ -185,11 +182,16 @@ class ddqn:
         mlflow.set_experiment("pacman")
         with mlflow.start_run() as run:
 
-            while self.gpu_stream.qsize() <= 3:
+            while self.gpu_stream.qsize() <= 10:
                 time.sleep(0.1)
 
-            for t in tqdm(range(Q1_NET_UPDATE_FREQ),total=Q1_NET_UPDATE_FREQ):    
-                s_state,s_nx_state,s_reward,s_done,s_action = self.gpu_stream.get() 
+            for t in tqdm(range(Q1_NET_UPDATE_FREQ),total=Q1_NET_UPDATE_FREQ):   
+                s_state,s_nx_state,s_reward,s_done,s_action = self.gpu_stream.get()
+                s_state = s_state.to(DEVICE)
+                s_nx_state = s_nx_state.to(DEVICE)
+                s_reward = s_reward.to(DEVICE)
+                s_done = s_done.to(DEVICE)
+                s_action = s_action.long().to(DEVICE)
                                
                 pred_q = self.q1(s_state).gather(1,s_action).squeeze()
                 
@@ -208,7 +210,7 @@ class ddqn:
                 if t > 0 and t % 2500 == 0: # update target net every 10k steps , 2500 = TARGET_NET_UPDATE_FREQ / 4
                     self.target_net.load_state_dict(self.q1.state_dict())
                         
-                if t > 0 and t % 1_000 == 0:
+                if t > 0 and t % 500 == 0:
                     self.save(n) 
                     mlflow.log_metrics({
                         "average reward":self.reward_data.mean().item(),
