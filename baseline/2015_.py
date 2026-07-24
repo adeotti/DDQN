4cdfcb252c8e1e5b6b77abbf71326e48ca1cc9f6
adeotti <wafickachiambi@gmail.com>
2026-07-23 20:43:14 -0400

    Add custom skip frames and update main sampler processor and step env method

--- baseline/2015_.py
+++ baseline/2015_.py
@@ -16,13 +16,14 @@ from collections import deque
 from tqdm import tqdm
 from threading import Thread,Event
 from queue import Queue
+from ratelimit import limits, sleep_and_retry
 
 
 MAX_EP_STEPS = 500
-NUM_ENVS = 10
+NUM_ENVS = 5
 R_SHAPE = (100,100)
 # -
-DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 MAX_STEPS = int(35e5)
 GAMMA = .99
 LR = 25e-5
@@ -32,11 +33,30 @@ TARGET_NET_UPDATE_FREQ = int(10e3)  # update q target weights every 10k steps
 BUFFER_SIZE = 100_000 // NUM_ENVS
 
 
+class SkipFrame(gym.Wrapper):
+    def __init__(self,env,skip):
+        super().__init__(env)
+        self.skip = skip
+
+    def step(self,action):
+        _rewards = 0
+        for n in range(self.skip):
+            observation,reward,done,truncation,info = self.env.step(action)
+            _rewards += reward
+    
+        return observation,_rewards,done,truncation,info
+
+    def reset(self,**kwargs):
+        observation,info = self.env.reset(**kwargs)
+        return observation,info
+
+
 def vec_env():
     def make():
         x = gym.make("ALE/MsPacman-v5",max_episode_steps=MAX_EP_STEPS)
         x = GrayscaleObservation(x)
         x = ResizeObservation(x,R_SHAPE)
+        x = SkipFrame(x,4)
         x = FrameStackObservation(x,4)
         return x 
     return AsyncVectorEnv([make for _ in range(NUM_ENVS)])
@@ -54,10 +74,9 @@ class q_function(nn.Module):
 
     def forward(self,s):
         x = s / 255.
-        x = self.c1(x)
+        x = F.silu(self.c1(x))
         x = F.silu(self.c2(x))
-        x = F.silu(self.c3(x))
-        
+        x = F.silu(self.c3(x)) 
         x = F.silu(self.l1(x.flatten(1)))
         return self.l2(x)
 
@@ -72,37 +91,35 @@ class ddqn:
         self.q1 = q_function()
         self.q1(dummy_obs)
         self.q1 = q_function().to(DEVICE)
+
+        self.q1_cpu = deepcopy(self.q1).cpu()
+        self.q1_cpu(dummy_obs)
+        self.q1_cpu.share_memory()
+        
         self.q1.compile(mode="max-autotune")
         
         self.target_net = deepcopy(self.q1).to(DEVICE)
-        for param in self.target_net.parameters():
-            param.requires_grad = False
 
         self.optim = torch.optim.Adam(self.q1.parameters(),lr=LR,fused=True)
         self.reward_data = torch.zeros(NUM_ENVS, dtype=torch.float)
         self.global_step = 0
         self.target_sync_event = Event() # tracking global steps
 
-        self.episode_queue = Queue(maxsize=5) # holds full raw episodes
-        self.batch_queue = Queue(maxsize=32)  # holds processed batches for GPU
+        self.episode_queue = Queue(maxsize=40) # holds full raw episodes
+        self.batch_queue = Queue(maxsize=125)  # holds processed batches for GPU
 
         self.thread_1 = Thread(target=self.step_env,args=(self.episode_queue,),daemon=True)
         self.thread_2 = Thread(target=self.sample_processor,args=(self.episode_queue,self.batch_queue),daemon=True)
 
-
-    def create_buffer(self):
-        return (
-            torch.zeros(MAX_EP_STEPS,NUM_ENVS,self.channels,*R_SHAPE),
-            torch.zeros(MAX_EP_STEPS,NUM_ENVS,self.channels,*R_SHAPE),
-            torch.zeros(MAX_EP_STEPS,NUM_ENVS),
-            torch.zeros(MAX_EP_STEPS,NUM_ENVS),
-            torch.zeros(MAX_EP_STEPS,NUM_ENVS,dtype=torch.int64)
-        )
-
-
+ 
     @torch.no_grad()
     def step_env(self,queue):
-        t_state,t_nx_state,t_reward,t_done,t_action = self.create_buffer()
+        t_state = torch.zeros(MAX_EP_STEPS,NUM_ENVS,self.channels,*R_SHAPE,dtype=torch.uint8)
+        t_nx_state = torch.zeros(MAX_EP_STEPS,NUM_ENVS,self.channels,*R_SHAPE,dtype=torch.uint8)
+        t_reward = torch.zeros(MAX_EP_STEPS,NUM_ENVS)
+        t_done = torch.zeros(MAX_EP_STEPS,NUM_ENVS,dtype=torch.bool)
+        t_action = torch.zeros(MAX_EP_STEPS,NUM_ENVS,dtype=torch.int64)
+
         state = torch.tensor(self.env.reset()[0],dtype=torch.float,device="cpu")
         n = 0
 
@@ -111,29 +128,30 @@ class ddqn:
             epsilon = 1 - (1 - 0.1) * decay_fraction
 
             if random.random() < epsilon: action = self.env.action_space.sample()
-            else: action = torch.argmax(self.q1(state.to(DEVICE)), dim=1).tolist()
+            else: action = torch.argmax(self.q1_cpu(state),dim=1).tolist()
 
             nx_state, reward, done, trunc, _ = self.env.step(action)
             self.reward_data += reward
 
             t_state[n].copy_(state)
-            t_nx_state[n].copy_(torch.tensor(nx_state))
-            t_reward[n].copy_(torch.tensor(reward))
-            t_done[n].copy_(torch.tensor(done))
-            t_action[n].copy_(torch.tensor(action))
+            t_nx_state[n].copy_(torch.as_tensor(nx_state))
+            t_reward[n].copy_(torch.as_tensor(reward))
+            t_done[n].copy_(torch.as_tensor(done))
+            t_action[n].copy_(torch.as_tensor(action))
+
+            state = torch.as_tensor(nx_state).float()
 
             n += 1
             self.global_step += 1
+
             if self.global_step > 0 and self.global_step % TARGET_NET_UPDATE_FREQ == 0:
                 self.target_sync_event.set()
 
             if n == MAX_EP_STEPS:
-                queue.put((t_state.clone(),t_nx_state.clone(),t_reward.clone(),t_done.clone(),t_action.clone()))
-                t_state,t_nx_state,t_reward,t_done,t_action = self.create_buffer()
+                queue.put((t_state,t_nx_state,t_reward,t_done,t_action))
                 n = 0
-
-            state = torch.tensor(nx_state,dtype=torch.float,device="cpu")
-
+                self.reward_data = torch.zeros(NUM_ENVS,dtype=torch.float)
+    
 
     def sample_processor(self,ep_queue,batch_queue):
         b_state = torch.zeros(BUFFER_SIZE,NUM_ENVS,self.channels,*R_SHAPE,dtype=torch.uint8)
@@ -141,13 +159,12 @@ class ddqn:
         b_reward = torch.zeros(BUFFER_SIZE,NUM_ENVS)
         b_done = torch.zeros(BUFFER_SIZE,NUM_ENVS,dtype=torch.bool)
         b_action = torch.zeros(BUFFER_SIZE,NUM_ENVS,dtype=torch.int64)
-
         ptr = 0
         size = 0
 
-        while True:
+        while True:           
             ep_state,ep_nx_state,ep_reward,ep_done,ep_action = ep_queue.get()
-
+            
             indices = torch.arange(ptr, ptr + MAX_EP_STEPS) % BUFFER_SIZE # circular buffer core indexing method 
             b_state[indices] = ep_state.to(torch.uint8)
             b_nx_state[indices] = ep_nx_state.to(torch.uint8)
@@ -157,23 +174,23 @@ class ddqn:
 
             ptr = (ptr + MAX_EP_STEPS) % BUFFER_SIZE
             size = min(size + MAX_EP_STEPS,BUFFER_SIZE)
-        
-            if size < 10_000:
-                continue
-            
+ 
             for _ in range(MAX_EP_STEPS//4): # sampling every 4 step, 500 steps -> 125 samples
+                time.sleep(0.03)
                 idx = torch.randint(0,size,(BATCH_SIZE,))
-                s_state = b_state[idx].flatten(0,1).to(torch.float32)
-                s_nx_state = b_nx_state[idx].flatten(0,1).to(torch.float32)
+                s_state = b_state[idx].flatten(0,1)
+                s_nx_state = b_nx_state[idx].flatten(0,1)
                 s_reward = b_reward[idx].reshape(-1)
-                s_done = b_done[idx].reshape(-1).to(torch.float32)
+                s_done = b_done[idx].reshape(-1)
                 s_action = b_action[idx].reshape(-1,1)
 
-                batch_queue.put((s_state,s_nx_state,s_reward,s_done,s_action))
-    
-    
+                items = (s_state,s_nx_state,s_reward,s_done,s_action)
+                batch_queue.put(items)
+                
+                
     def save(self,n): 
-        data = { "q1 state":self.q1.state_dict(),
+        data = { 
+            "q1 state":self.q1.state_dict(),
             "target net state":self.target_net.state_dict(),
             "optim state":self.optim.state_dict()
         }
@@ -190,28 +207,27 @@ class ddqn:
     
 
     def main(self):
-        self.thread_1.start()
-        self.thread_2.start()
-
         mlflow.set_experiment("pacman")
         with mlflow.start_run() as run:
 
-            while self.batch_queue.qsize() < 10:
-                time.sleep(0.1)
+            self.thread_1.start()
+            while self.episode_queue.qsize() < 30: 
+                time.sleep(0.01)
 
-            for t in tqdm(range((MAX_STEPS//MAX_EP_STEPS) + 1),total=(MAX_STEPS//MAX_EP_STEPS) + 1):
+            self.thread_2.start()
+  
+            for t in tqdm(range((40_000) + 1),total=(40_000) + 1):
                 s_state,s_nx_state,s_reward,s_done,s_action = self.batch_queue.get()
-                
-                s_state = s_state.to(DEVICE,non_blocking=True)
-                s_nx_state = s_nx_state.to(DEVICE,non_blocking=True)
+
+                s_state = s_state.to(DEVICE,torch.float32,non_blocking=True)
+                s_nx_state = s_nx_state.to(DEVICE,torch.float32,non_blocking=True)
                 s_reward = s_reward.to(DEVICE,non_blocking=True)
-                s_done = s_done.to(DEVICE,non_blocking=True)
+                s_done = s_done.to(DEVICE,torch.float32,non_blocking=True)
                 s_action = s_action.to(DEVICE,non_blocking=True)
 
                 with torch.amp.autocast(device_type="cuda",dtype=torch.bfloat16):
                     pred_q = self.q1(s_state).gather(1,s_action).squeeze(1)
                     loss = self.compute_loss(s_nx_state,s_reward,s_done,pred_q)
-                    current_loss = loss.item()
 
                 self.optim.zero_grad(set_to_none=True)
                 loss.backward()
@@ -219,19 +235,17 @@ class ddqn:
 
                 if self.target_sync_event.is_set():
                     self.target_net.load_state_dict(self.q1.state_dict())
-                    self.target_sync_event.clear() # reset the flag !!
-
-                if t > 0 and t % 500 == 0:
-                    self.save(t)
-
-                    mlflow.log_metrics(
-                        {
+                    self.target_sync_event.clear() 
+                
+                if t > 0 and t % 2_000 == 0: 
+                    mlflow.log_metrics({
                         "average reward": self.reward_data.mean().item(),
                         "loss": current_loss,
-                        },
-                    step=self.global_step
+                        },step=self.global_step
                     ) 
-                    self.reward_data = torch.zeros(NUM_ENVS,dtype=torch.float) 
+                    self.reward_data = torch.zeros(NUM_ENVS,dtype=torch.float)
+            
+            self.save(t)
 
     
     def test(self):
@@ -242,7 +256,7 @@ class ddqn:
         state = env.reset()[0]
         
         policy = q_function()
-        checkpoint = torch.load("./state_7000.pth", map_location=torch.device("cpu"))
+        checkpoint = torch.load("./state_40000.pth", map_location=torch.device("cpu"))
         compiled_state_dict = {k.replace("_orig_mod.", ""): v for k, v in checkpoint["q1 state"].items()}
         policy.load_state_dict(compiled_state_dict)
 
@@ -259,5 +273,5 @@ class ddqn:
 if __name__ == "__main__": 
     import warnings,logging
     warnings.filterwarnings("ignore") ; logging.disable(logging.CRITICAL)
-    ddqn("./").test()
+    ddqn("./").main()
     
